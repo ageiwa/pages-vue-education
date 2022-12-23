71d227eaf39aafef89ac930f978f84059aa34ea1
Vlad Ageysha <ander123fakk@gmail.com>
2022-12-23 17:16:04 +0300

    Transform input in div

--- src/components/PageTwo.vue
+++ src/components/PageTwo.vue
@@ -1,23 +1,29 @@
 <script setup>
-    import { ref, watch } from 'vue'
+    import { ref } from 'vue'
     import { useTitleStore } from '../stores/title';
 
     const storeTitle = useTitleStore()
     const title = ref(storeTitle.title)
 
-    watch(title, (newTitle) => {
-        storeTitle.change(newTitle)
-    })
+    function onInput(e) {
+        title.value = e.target.innerText
+        storeTitle.change(e.target.innerText)
+    }
 
 </script>
 
 <template>
     <h1>Hello Page two</h1>
-    <input v-model="title">
+    <div ref="divElem" @keyup="onInput" class="input-div" contenteditable="true">{{ title }}</div>
     <br>
     <RouterLink to="/">Перейти на первую</RouterLink>
 </template>
 
 <style scoped>
-
+    .input-div {
+        border: 1px solid #fff;
+        border-radius: 5px;
+        widows: 100%;
+        height: 20px;
+    }
 </style>
